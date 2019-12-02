da5c887ae050fb76c84b75f7e4cefc6dbe96906f
Samuel Cozannet <samuel.cozannet@madeden.com>
2019-12-02 11:15:03 +0100

    fix credentials access

--- launch.sh
+++ launch.sh
@@ -23,8 +23,8 @@ check_env(){
 
 write_aws_credentials() {
   sed \
-    -e "s/AWS_ACCESS_KEY_ID/$AWS_ACCESS_KEY_ID/g" \
-    -e "s/AWS_SECRET_ACCESS_KEY/$AWS_SECRET_ACCESS_KEY/g" \
+    -e 's/AWS_ACCESS_KEY_ID/'$AWS_ACCESS_KEY_ID'/g' \
+    -e 's/AWS_SECRET_ACCESS_KEY/'$AWS_SECRET_ACCESS_KEY'/g' \
     credentials.template > "${AWS_SHARED_CREDENTIALS_FILE}"
 }
 
@@ -32,8 +32,8 @@ write_aws_main_config() {
   local CROSSACCOUNT_ROLE="$1"
   local LIST_ACCOUNTS_ROLE="$2"
   sed \
-    -e "s/CROSSACCOUNT_ROLE/$CROSSACCOUNT_ROLE/g" \
-    -e "s/LIST_ACCOUNTS_ROLE/$LIST_ACCOUNTS_ROLE/g" \
+    -e 's/CROSSACCOUNT_ROLE/'$CROSSACCOUNT_ROLE'/g' \
+    -e 's/LIST_ACCOUNTS_ROLE/'$LIST_ACCOUNTS_ROLE'/g' \
     config.template > "${AWS_CONFIG_FILE}"
 }
 
